29c38cd54131e7984208bbddc5128eb2302013f8
Tim Dawson <tjdawson@sas.upenn.edu>
2016-04-19 16:57:52 -0400

    initial
    commit

--- data_includes/experiment.js
+++ data_includes/experiment.js
@@ -1,145 +1,323 @@
-// functions for generating followup links and emails
-
-var email;
-var unique_id;
-var followup_link;
-
-function send_mail(){
-    email = document.getElementById('participant_email');
-    unique_id = document.getElementById('unique_id');
-    followup_link = "http://spellout.net/ibexexps/trueswell_lab/followup/"+unique_id.value+"/experiment.html";
-    document.location.href = "mailto:"+email.value+"?subject=Sleep Study Followup Survey Link&body="+followup_link;
-}
-
-function print_link(){
-    email = document.getElementById('participant_email');
-    unique_id = document.getElementById('unique_id');
-    followup_link = "http://spellout.net/ibexexps/trueswell_lab/followup"+unique_id.value+"/experiment.html";
-    var display_link = document.createElement('p');
-    display_link.textContent = followup_link;
-    document.getElementById("display_link").appendChild(display_link)
-}
-
-//
-
-var shuffleSequence = seq("intro", "intro1", ""
-                          );
+var manualSendResults = true; 
+var shuffleSequence = seq("intro", "intro1", "sep", 
+"test_bist_funny_highest","sep",
+"test_tace_funny_distractor","sep",
+"test_zant_push_distractor","sep",
+"test_jair_time_target","sep",
+"test_tiz_in_distractor","sep",
+"test_tiz_play_highest","sep",
+"test_jair_eat_highest","sep",
+"test_mipen_cup_lowest","sep",
+"test_telpen_play_lowest","sep",
+"test_zant_book_target","sep",
+"test_blime_dad_lowest","sep",
+"test_doon_yay_highest","sep",
+"test_bist_read_distractor","sep",
+"test_telpen_funny_distractor","sep",
+"test_geck_funny_distractor","sep",
+"test_tiz_thing_target","sep",
+"test_blime_toy_target","sep",
+"test_geck_open_lowest","sep",
+"test_jair_stand_lowest","sep",
+"test_bist_horse_target","sep",
+"test_tiz_me_lowest","sep",
+"test_tace_hello_lowest","sep",
+"test_doon_baby_lowest","sep",
+"test_zant_read_highest","sep",
+"test_mipen_bag_target","sep",
+"test_geck_nose_target","sep",
+"test_tula_here_highest","sep",
+"test_tace_phone_target","sep",
+"test_tula_eat_distractor","sep",
+"test_tace_look_highest","sep",
+"test_vash_truck_highest","sep",
+"test_mipen_throw_distractor","sep",
+"test_tula_me_lowest","sep",
+"test_geck_dad_highest","sep",
+"test_telpen_ball_target","sep",
+"test_zant_box_lowest","sep",
+"test_bist_look_lowest","sep",
+"test_jair_in_distractor","sep",
+"test_tula_mommy_target","sep",
+"test_vash_look_distractor","sep",
+"test_mipen_in_highest","sep",
+"test_blime_play_distractor","sep",
+"test_blime_push_highest","sep",
+"test_doon_eat_distractor","sep",
+"test_doon_necklace_target","sep",
+"test_vash_shoe_target","sep",
+"test_telpen_throw_highest","sep",
+"test_vash_purse_lowest","outro1","sr","outro2"
+);
 
 var defaults = [
-    "Separator", {
-        transfer: "keypress",
-        normalMessage: "Please look up at the display on the wall and watch the video. When it's finished, press any key to start guessing.",
-        errorMessage: "Please wait for the video to finish. Then, press any key to start guessing."
-    },
-    "DashedSentence", {
-        mode: "self-paced reading"
-    },
-    "AcceptabilityJudgment", {
-        as: ["1", "2", "3", "4", "5", "6", "7"],
-        presentAsScale: true,
-        instructions: "Use number keys or click boxes to answer.",
-        leftComment: "(Bad)", rightComment: "(Good)"
-    },
-    "Question", {
-        hasCorrect: true
-    },
-    "Message", {
-        hideProgressBar: true
-    },
-    "Form", {
-        hideProgressBar: false,
-        continueOnReturn: true,
-        saveReactionTime: false
+        "Separator", {
+            transfer: "keypress",
+            normalMessage: "Press any key to continue.",
+            errorMessage: "Press any key to continue."
+        },
+        "DashedSentence", {
+            mode: "self-paced reading"
+        },
+        "AcceptabilityJudgment", {
+            as: ["1", "2", "3", "4", "5", "6", "7"],
+            presentAsScale: true,
+            instructions: "Use number keys or click boxes to answer.",
+            leftComment: "(Bad)", rightComment: "(Good)"
+        },
+        "Question", {
+            hasCorrect: true
+        },
+        "Message", {
+            hideProgressBar: true
+        },
+        "Form", {
+            hideProgressBar: false,
+            continueOnReturn: true,
+            saveReactionTime: false
+        }
+    ];
+
+var items = [    ["sr", "__SendResults__", { }],
+["sep", "Separator", { }],
+        
+        //
+        
+        ["intro", "Form", {
+            html: {include: "intro.html"},
+            hideProgressBar: true,
+            countsForProgressBar: false
+        
+        }],
+        
+        //
+        
+        ["intro1", "Form", {
+            html: {include: "intro1.html"},
+            hideProgressBar: true,
+            countsForProgressBar: false,
+    validators: {
+    audiotest: function(s)
+    { if (s == "3401")
+    return true; else return "Incorrect entry for \u2018test numbers\u2019";}
     }
-];
+        
+        }],
+        
+        //
+        
+        ["outro1", "Form", {
+            html: {include: "outro1.html"},
+            hideProgressBar: true,
+            countsForProgressBar: false
+        
+        }],
+        
+        //
+        
+        ["outro2", "Form", {
+            html: { include: "outro2.html" },
+            hideProgressBar: true,
+            countsForProgressBar: false,
+        } ],
+        
+        //
+["test_tula_mommy_target", "Form", { 
+html: {include: "test_tula_mommy_target.html"} 
+}],
 
-var items = [
+["test_tula_here_highest", "Form", { 
+html: {include: "test_tula_here_highest.html"} 
+}],
 
-    ["sep", "Separator", { }],
+["test_tula_me_lowest", "Form", { 
+html: {include: "test_tula_me_lowest.html"} 
+}],
 
-    //
+["test_tula_eat_distractor", "Form", { 
+html: {include: "test_tula_eat_distractor.html"} 
+}],
 
-    ["end", "Form", {
-        html: {include: "end.html"},
-        hideProgressBar: true,
-        countsForProgressBar: false
+["test_vash_shoe_target", "Form", { 
+html: {include: "test_vash_shoe_target.html"} 
+}],
 
-    }],
+["test_vash_truck_highest", "Form", { 
+html: {include: "test_vash_truck_highest.html"} 
+}],
 
-    //
+["test_vash_purse_lowest", "Form", { 
+html: {include: "test_vash_purse_lowest.html"} 
+}],
 
-    ["intro", "Form", {
-        html: {include: "intro.html"},
-        hideProgressBar: true,
-        countsForProgressBar: false
+["test_vash_look_distractor", "Form", { 
+html: {include: "test_vash_look_distractor.html"} 
+}],
 
-    }],
+["test_geck_nose_target", "Form", { 
+html: {include: "test_geck_nose_target.html"} 
+}],
 
-    //
+["test_geck_dad_highest", "Form", { 
+html: {include: "test_geck_dad_highest.html"} 
+}],
 
-    ["intro1", "Form", {
-        html: {include: "intro1.html"},
-        hideProgressBar: true,
-        countsForProgressBar: false,
-        validators: {
-            audiotest: function (s) { if (s == "3401") return true; else return "Incorrect entry for \u2018test numbers\u2019"; }
-        }
-    }],
+["test_geck_open_lowest", "Form", { 
+html: {include: "test_geck_open_lowest.html"} 
+}],
 
-    //
+["test_geck_funny_distractor", "Form", { 
+html: {include: "test_geck_funny_distractor.html"} 
+}],
 
-    ["intro2", "Form", {
-        html: {include: "intro2.html"},
-        hideProgressBar: true,
-        countsForProgressBar: false
+["test_tiz_thing_target", "Form", { 
+html: {include: "test_tiz_thing_target.html"} 
+}],
 
-    }],
+["test_tiz_play_highest", "Form", { 
+html: {include: "test_tiz_play_highest.html"} 
+}],
 
-    //
+["test_tiz_me_lowest", "Form", { 
+html: {include: "test_tiz_me_lowest.html"} 
+}],
 
-    ["intro3", "Form", {
-        html: { include: "intro3.html" },
-        hideProgressBar: true,
-        countsForProgressBar: false,
-        validators: {
-            age: function (s) { if (s.match(/^\d+$/)) return true; else return "Bad value for \u2018age\u2019"; }
-        }
-    } ],
+["test_tiz_in_distractor", "Form", { 
+html: {include: "test_tiz_in_distractor.html"} 
+}],
 
-    //
+["test_bist_horse_target", "Form", { 
+html: {include: "test_bist_horse_target.html"} 
+}],
 
-    ["guess0", "Form", {
-        html: { include: "guess0.html" }
-    } ],
+["test_bist_funny_highest", "Form", { 
+html: {include: "test_bist_funny_highest.html"} 
+}],
 
-    //
+["test_bist_look_lowest", "Form", { 
+html: {include: "test_bist_look_lowest.html"} 
+}],
 
-    ["guess1", "Form", {
-        html: { include: "guess.html" }
-    } ],
+["test_bist_read_distractor", "Form", { 
+html: {include: "test_bist_read_distractor.html"} 
+}],
 
-    //
+["test_jair_time_target", "Form", { 
+html: {include: "test_jair_time_target.html"} 
+}],
 
-    ["guess2", "Form", {
-        html: { include: "guess.html" }
-    } ],
+["test_jair_eat_highest", "Form", { 
+html: {include: "test_jair_eat_highest.html"} 
+}],
 
-    //
+["test_jair_stand_lowest", "Form", { 
+html: {include: "test_jair_stand_lowest.html"} 
+}],
 
-    ["guess3", "Form", {
-        html: { include: "guess.html" }
-    } ],
+["test_jair_in_distractor", "Form", { 
+html: {include: "test_jair_in_distractor.html"} 
+}],
 
-    //
+["test_tace_phone_target", "Form", { 
+html: {include: "test_tace_phone_target.html"} 
+}],
 
-    ["guess4", "Form", {
-        html: { include: "guess.html" }
-    } ],
+["test_tace_look_highest", "Form", { 
+html: {include: "test_tace_look_highest.html"} 
+}],
 
-    //
+["test_tace_hello_lowest", "Form", { 
+html: {include: "test_tace_hello_lowest.html"} 
+}],
 
-    ["sep1", "Separator", {
-        normalMessage: "Please look up at the display on the wall and wait for the next video. Then, press any key to begin guessing.",
-    }]
+["test_tace_funny_distractor", "Form", { 
+html: {include: "test_tace_funny_distractor.html"} 
+}],
 
-];
+["test_doon_necklace_target", "Form", { 
+html: {include: "test_doon_necklace_target.html"} 
+}],
+
+["test_doon_yay_highest", "Form", { 
+html: {include: "test_doon_yay_highest.html"} 
+}],
+
+["test_doon_baby_lowest", "Form", { 
+html: {include: "test_doon_baby_lowest.html"} 
+}],
+
+["test_doon_eat_distractor", "Form", { 
+html: {include: "test_doon_eat_distractor.html"} 
+}],
+
+["test_telpen_ball_target", "Form", { 
+html: {include: "test_telpen_ball_target.html"} 
+}],
+
+["test_telpen_throw_highest", "Form", { 
+html: {include: "test_telpen_throw_highest.html"} 
+}],
+
+["test_telpen_play_lowest", "Form", { 
+html: {include: "test_telpen_play_lowest.html"} 
+}],
+
+["test_telpen_funny_distractor", "Form", { 
+html: {include: "test_telpen_funny_distractor.html"} 
+}],
+
+["test_blime_toy_target", "Form", { 
+html: {include: "test_blime_toy_target.html"} 
+}],
+
+["test_blime_push_highest", "Form", { 
+html: {include: "test_blime_push_highest.html"} 
+}],
+
+["test_blime_dad_lowest", "Form", { 
+html: {include: "test_blime_dad_lowest.html"} 
+}],
+
+["test_blime_play_distractor", "Form", { 
+html: {include: "test_blime_play_distractor.html"} 
+}],
+
+["test_zant_book_target", "Form", { 
+html: {include: "test_zant_book_target.html"} 
+}],
+
+["test_zant_read_highest", "Form", { 
+html: {include: "test_zant_read_highest.html"} 
+}],
+
+["test_zant_box_lowest", "Form", { 
+html: {include: "test_zant_box_lowest.html"} 
+}],
+
+["test_zant_push_distractor", "Form", { 
+html: {include: "test_zant_push_distractor.html"} 
+}],
+
+["test_mipen_bag_target", "Form", { 
+html: {include: "test_mipen_bag_target.html"} 
+}],
+
+["test_mipen_in_highest", "Form", { 
+html: {include: "test_mipen_in_highest.html"} 
+}],
+
+["test_mipen_cup_lowest", "Form", { 
+html: {include: "test_mipen_cup_lowest.html"} 
+}],
+
+["test_mipen_throw_distractor", "Form", { 
+html: {include: "test_mipen_throw_distractor.html"} 
+}],
+
+["end", "Form", {
+            html: {include: "end.html"},
+            hideProgressBar: true,
+            countsForProgressBar: false
+        
+        }]
+    ];
